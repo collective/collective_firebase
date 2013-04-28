af62c985e9208173435e79a6c83b194152e1160e
Balazs Ree <ree@greenfinity.hu>
2013-04-28 14:38:27 +0200

    Make angular's js shipped in a minified form, as Plone's own resource registry compression is broken and angular triggers its bugs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,15 @@ module.exports = function(grunt) {
         }
       }
     },
+    uglify: {
+      'default': {
+        files: {
+          'plone_interact/static/dist/angular.min.js': [
+            collect.bower('angular').path('angular.js')
+          ]
+        }
+      }
+    },
     watch: {
       options: {
         debounceDelay: 250
@@ -27,16 +36,17 @@ module.exports = function(grunt) {
           collect.bower('angularFire').path('angularFire.js'),
           collect.bower('angular-elastic').path('elastic.js')
         ],
-        tasks: ['copy:default']
+        tasks: ['copy:default', 'uglify:default']
       }
     }
   });
 
   // Load the task plugins.
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['copy:default']);
+  grunt.registerTask('default', ['copy:default', 'uglify:default']);
 
 };
