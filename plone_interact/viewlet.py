5e21bef27a8b138be3b420b9776fb7ce911b875e
Balazs Ree <ree@greenfinity.hu>
2013-05-10 11:24:16 +0200

    Modify the viewlet's condition to not render when the product is not installed to Plone. This fixes the problem of a broken viewlet, when the python package is already installed, and a new portal is created where the product has not been installed yet with genericsetup / quickinstaller.

--- plone_interact/viewlet.py
+++ plone_interact/viewlet.py
@@ -1,6 +1,7 @@
 
 # Copyright (c) 2013 Enfold Systems, Inc. All rights reserved.
 
+from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets import common as base
 
 from .config import get_config
@@ -15,3 +16,17 @@ class InteractViewlet(base.ViewletBase):
         super(InteractViewlet, self).update()
         self.auth_token = get_auth_token(self.context, self.request)
         self.config = get_config()
+        # Is the product installed?
+        # We need to check it, as this information is not obvious based on
+        # the configuration alone.
+        # The importance of this check is to avoid the viewlet being
+        # rendered before the product is installed with quickinstaller
+        # in a newly created portal.
+        qi = getToolByName(self.context, 'portal_quickinstaller')
+        self.is_installed = qi.isProductInstalled('plone_interact')
+
+    def render(self):
+        if self.is_installed and self.auth_token:
+            return super(InteractViewlet, self).render()
+        else:
+            return ''
